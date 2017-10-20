d12e1eb61916d12226472dbbe065079c3e9f6a42
Melvin-88 <taisen747@gmail.com>
2017-10-20 08:18:46 +0300

    add 404 page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router';
 import App from '../containers/App';
+import {
+    Route,
+    Switch,
+    Redirect
+} from 'react-router-dom';
 import MainPage from '../containers/MainPage/MainPage';
+import NoMatch from '../containers/NoMatch/NoMatch';
+import Authentication from '../containers/Authentication/Authentication';
 
 export default (
     <App>
         <div>
-            <Route exact={true} path='/' component={MainPage}/>
-            {/*<Route exact={true} path='/simulation' component={PageSimulation} />*/}
+            <Switch>
+                <Route path='/' exact render={() => <Redirect to="/dashboard" push />}/>
+                <Route path='/dashboard' component={MainPage} />
+                <Route path='/authentication' component={Authentication} />
+                <Route component={NoMatch}/>
+            </Switch>
         </div>
     </App>
 )
